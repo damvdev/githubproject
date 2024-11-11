524449c10eada6babf0bb214375d69378d474faf
“Raquel <raquel.alaman@itb.cat>
2024-11-11 19:31:53 +0100

    Added suma recursiva. Closes #1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("HelloWorld");
+﻿using System;
+namespace Activities
+{
+	public class Program
+	{
+		public static int SumaIt(int n)
+		{
+			int num = 0;
+			for (int i = 0; i <= n; i++)
+			{
+				num += i;
+			}
+			return num;
+		}
+
+		public static int SumaRec(int n)
+		{
+			if (n == 0) return 0;
+			return n + SumaRec(n - 1);
+		}
+		public static void Main()
+		{
+			//Exemples suma iterativa i recursiva
+			Console.WriteLine(SumaIt(4));
+			Console.WriteLine(SumaRec(4));
+		}
+	}
+}
